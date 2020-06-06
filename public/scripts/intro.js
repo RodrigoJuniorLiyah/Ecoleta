87446bcb620cface4fca40cbd3d78d81fb2fb1e4
Rodrigo Junior <rodrigo.junior7@outlook.com>
2020-06-06 16:27:27 -0300

    Projeto concluído

---
+++ public/scripts/intro.js
@@ -0,0 +1,59 @@
+// comments
+// document.write("hellow")
+
+// variaveis, tipos de dados
+
+// document.write(myconst + myvar)
+
+// const s1 = "Isso é uma string"
+// const s2 = "isso também é uma string"
+// const s3 = `Isso é uma string também`
+
+// number
+// const n1 = '1'
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFaçse = false
+
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+ // altura: "1,80m",
+ // idade: 24,
+ // solteiro: true,
+ // correr(){
+// return "executar uma grande logica de correr"
+// }
+//}
+
+// document.write(pessoa.correr())
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: // "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// registrar funcao
+// function sayMyName(name) {
+// document.write(name)
+// }
+
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ) {
+// document.write("Reprovado")
+// } else {
+// document.write("aprovado")
+// }
+
+// let teste = 4
+
+// for (teste; teste < 10; teste++) {
+// document.write(`<p>${teste + " iae"}</p>`)
+// }
